9d87c315d1c7cbffae5ac1e31790f026c7721037
Sohail Ansari <sssooobetter@gmail.com>
2026-09-17 14:31:52 +0530

    Fix greeting message in main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class PredictionResponse(BaseModel):
 
 @app.get('/')
 def greet():
-    return {'Welcome to Sohail 's ML Predictor'}
+    return {'Welcome to Sohail 's ML-Predictor'}
 
 
 @app.post('/predict', response_model=PredictionResponse) #6.77777
